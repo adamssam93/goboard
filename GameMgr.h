b136689ffaeadde946f4e6eb77b6760c15acf10c
adamssam93 <sam.jv.adams@gmail.com>
2015-05-09 15:00:45 -0700

    Update GameMgr.h

--- GameMgr.h
+++ GameMgr.h
@@ -5,15 +5,8 @@ const char EMPTY = '\0';
 const char X = 'X';
 const char O = 'O';
 
-const int XBALLSTART = 177;
-const int YBALLSTART = 277;
-const int XBALLINC = 3;
-const int YBALLINC = 3;
-const int BALLWIDTH = 20;
-const int BALLHEIGHT = 20;
-
 extern char board[4][4];
-extern bool xMove;
+extern bool wMove;
 extern bool gameOver;
 extern int xRect, yRect; //Rectangle to draw the ball in
 extern int xInc, yInc; //How much to move the ball with each tick
@@ -24,8 +17,14 @@ void GameReset();
 void GameDrawBoard(HWND hwdn, HDC hdc); //Draws the game board with Xs and Os
 void GameSetMove(int i, int j); //Set the next move
 void GameCheckWinner(HWND hwnd); 
-void GameDrawX (HDC hdc, int i, int j);
-void GameDrawO(HDC hdc, int i, int j);
-void GameDrawBall(HWND hwnd, HDC hdc);
-void GameCreateFont();
+void GameDrawWhite (HDC hdc, int i, int j);
+void GameDrawBlack(HDC hdc, int i, int j);
 void GameSetKey(WPARAM wParam);
+void CheckLiberties(int i, int j);
+void GameOver(HWND hwnd, HDC hdc);
+
+//What we need:
+//Update liberties function so it will work for more than one piece being surrounded at a time, capture a piece if it's on the edge
+//Figure out how to TextOut values of variables
+//Display winner once the game has ended
+//Add a rules section in menu
